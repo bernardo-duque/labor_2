4552bf8ea2b9f509a8ad768ed1730b25676e01d2
bernardo-duque <66139258+bernardo-duque@users.noreply.github.com>
2022-11-04 18:05:53 -0300

    Add files via upload

--- Code/codigo_trabalho_II.R
+++ Code/codigo_trabalho_II.R
@@ -26,7 +26,7 @@ toc() #10-13 min
 
 # criando amostra para faciliar tratamento
 
-amostra <- sample_n(df,size = 10000,replace = F)
+#amostra <- sample_n(df,size = 100000,replace = F)
 
 #save(amostra, file = "amostra_base_filiacao.RData")
 #save(df,file = "base_filiacao.RData")
@@ -46,53 +46,63 @@ amostra <- amostra %>%
          data_cancelamento = ifelse(is.na(data_cancelamento) == T & is.na(data_desfiliacao) == T,
                                     as.Date("2021-01-01"),
                                     as.Date(data_cancelamento)),
-         data_saida = ifelse(data_desfiliacao > data_cancelamento, zoo::as.Date(data_desfiliacao),
-                             zoo::as.Date(data_cancelamento)))
+         data_saida = ifelse(is.na(data_desfiliacao),data_cancelamento,
+                             ifelse(is.na(data_cancelamento),data_desfiliacao,
+                                    ifelse(data_desfiliacao > data_cancelamento,data_desfiliacao,
+                                           data_cancelamento))))
 
 amostra <- amostra %>% 
-  mutate(data_desfiliacao = as.Date(data_desfiliacao),
-         data_cancelamento = as.Date(data_cancelamento),
-         data_saida = as.yearmon(as.Date(data_saida)))
-
-
-# criando dummy para situacao regular e coluna para mes e ano de saida
-
-amostra <- amostra %>%
-  mutate(situacao_registro = ifelse(situacao_registro == "REGULAR",1,0),
-         mes_saida = format(data_saida, "%m"),
-         ano_saida = format(data_saida, "%Y")) %>%
-  select(-data_desfiliacao,-data_cancelamento)
+  mutate(data_saida = as.Date(data_saida),
+         titulo_eleitoral = as.numeric(titulo_eleitoral)) %>%
+  select(-data_cancelamento,-data_desfiliacao)
 
 # definindo a virada do ano como outubro para captar mudancas pre eleitorais
 
 amostra <- amostra %>%
-  mutate(ano_saida = as.numeric(ano_saida), mes_saida = as.numeric(mes_saida),
-         ano_saida = ifelse(mes_saida>10,ano_saida + 1,ano_saida),
-         titulo_eleitoral = as.numeric(titulo_eleitoral))
+  mutate(ano_2013 = ifelse(data_filiacao < "2013-11-01",1,0),
+         ano_2014 = ifelse(data_filiacao < "2014-11-01" &
+                             data_saida >= "2013-10-01", 1,0),
+         ano_2015 = ifelse(data_filiacao < "2015-11-01" &
+                             data_saida >= "2014-11-01", 1,0),
+         ano_2016 = ifelse(data_filiacao < "2016-11-01" &
+                             data_saida >= "2015-11-01", 1,0),
+         ano_2017 = ifelse(data_filiacao < "2017-11-01" &
+                             data_saida >= "2016-11-01", 1,0),
+         ano_2018 = ifelse(data_filiacao < "2018-11-01" &
+                             data_saida >= "2017-11-01", 1,0),
+         ano_2019 = ifelse(data_filiacao < "2019-11-01" &
+                             data_saida >= "2018-11-01", 1,0),
+         ano_2020 = ifelse(data_filiacao < "2020-11-01" &
+                             data_saida >= "2019-11-01", 1,0))
 
-colunas_anos <- vector(length = 8)
-for (i in 2013:2020) {
-  colunas_anos[i-2012] <- paste0("ano_",i)
-  amostra[,colunas_anos[i-2012]] <- 0
-}
+# analisando o caso de mais de um partido por pessoa no mesmo ano
 
-base_ano <- amostra %>%
-  mutate(ano_2013 = ifelse(ano_saida >= 2013,1,0),
-         ano_2014 = ifelse(ano_saida >= 2014,1,0),
-         ano_2015 = ifelse(ano_saida >= 2015,1,0),
-         ano_2016 = ifelse(ano_saida >= 2016,1,0),
-         ano_2017 = ifelse(ano_saida >= 2017,1,0),
-         ano_2018 = ifelse(ano_saida >= 2018,1,0),
-         ano_2019 = ifelse(ano_saida >= 2019,1,0),
-         ano_2020 = ifelse(ano_saida >= 2020,1,0))
+print(paste0("Há ",round(((nrow(amostra) - length(unique(amostra$nome)))/nrow(amostra))*100,2),
+             "% de indivíduos com mais de um partido na amostra"))
 
-### FALTA LIMPAR PARA O CASO DE MAIS DE UM PARTIDO POR PESSOA AQUI
+# removendo esses individuos
 
-partidos_ano <- base_ano %>%
+titulos <- which(duplicated(amostra$titulo_eleitoral))
+
+amostra <- amostra %>%
+  filter(titulo_eleitoral %nin% titulos)
+
+# guardando os nomes para se retirar na base dos servidores
+
+nomes_retirar <- amostra %>%
+  filter(titulo_eleitoral %in% titulos) %>%
+  select(nome) %>%
+  pull
+
+rm(titulos)
+
+# montando as bases finais para plotar
+
+partidos_ano <- amostra %>%
   group_by(sigla_partido) %>%
   summarise(across(ano_2013:ano_2020, ~sum(.x)))
 
-base_ano <- base_ano %>%
+base_ano <- amostra %>%
   summarise(across(ano_2013:ano_2020, ~sum(.x)))
 
 base_ano <- base_ano %>%
@@ -100,17 +110,8 @@ base_ano <- base_ano %>%
 
 base_ano$ano <- as.numeric(str_remove(base_ano$ano,"ano_"))
 
-save(base_ano,partidos_ano,"bases_tratadas.RData")
-
-
-source('/Users/bernardoduque/Documents/Puc/theme_publication_ggplot.R')
-
-quartz()
-grafico_1 <- ggplot(base_ano,aes(ano,num_filiados))+
-  geom_line(size=1.3) + geom_point() +
-  xlab("Ano") + ylab("Number of Affiliates") + 
-  scale_colour_brewer() + theme_clean() +
-  scale_y_continuous(labels = label_number(accuracy = 1000))
+setwd("/Users/bernardoduque/Documents/Puc/Trabalho II/Trabalho Final/Output")
+save(base_ano,partidos_ano,file = "bases_tratadas.RData")
 
 g1 <- ggplot(base_ano,aes(x=ano,y=num_filiados)) + 
   geom_line(color = "dodgerblue") +
@@ -124,26 +125,22 @@ g1 <- ggplot(base_ano,aes(x=ano,y=num_filiados)) +
         panel.grid.minor.y = element_blank(),
         axis.text = element_text(face = "bold")) 
 
-line <- ggplot(base_ano,aes(ano,num_filiados)) + 
-  geom_line(size=1.3) + geom_point()
-grid.arrange(Line,(Line +scale_colour_Publication()+ theme_Publication()),nrow=1)
+  
+## plotando os partidos com mais numero de filiados
 
+partidos_ano <- partidos_ano %>%
+  filter(sigla_partido %in% c("MDB","PT","PSDB","PP","PDT"))
 
-save(grafico_1,file = "grafico_1.RData")
+# invertendo a df para plotar
+partidos_ano <- transpose(partidos_ano)
+colnames(partidos_ano) <- partidos_ano[1,]
+partidos_ano <- partidos_ano[2:nrow(partidos_ano),]
+anos <- 2013:2020
+partidos_ano <- cbind(anos,partidos_ano)
+
+# plotando
 
   
-## PEGAR OS 5 PARTIDOS COM MAIS FILIADOS E PIVOTLONGER
-  
-
-# amostra %>% mutate(n=1) %>% group_by(titulo_eleitoral) %>% summarise(contagem = sum(n)) mais de um partido
-
-
-  
-  
-seq(from = 2013,to = as.numeric(amostra$ano_saida[5]), by = 1)
-seq(2013,ano_saida,1)
-  
-
 #####----- Base Servidores -----#####
 
 servidores <- read.csv("servidores_federais_2013-2020.csv")
@@ -215,7 +212,7 @@ total_ano <- total_ano %>%
   pivot_longer(everything(),names_to = "ano",values_to = "num_empreg") %>%
   mutate(ano = as.integer(str_remove(ano,"ano_")))
 
-g2 <- ggplot(total_ano,aes(x=ano,y=num_empreg)) + 
+g3 <- ggplot(total_ano,aes(x=ano,y=num_empreg)) + 
   geom_line(color = "dodgerblue") +
   geom_point(aes(y=num_empreg),color = "dodgerblue") +
 #  geom_text(aes(label = num_empreg),vjust = -0.8)+
