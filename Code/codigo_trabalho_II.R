04626eba44a900df235303352a22c2d0ad09dbb0
bernardo-duque <66139258+bernardo-duque@users.noreply.github.com>
2022-11-04 15:31:40 -0300

    Add files via upload

--- Code/codigo_trabalho_II.R
+++ Code/codigo_trabalho_II.R
@@ -4,7 +4,9 @@ pacman::p_load(sf, tidyverse, geobr, ggplot2, terra, spData,
                tictoc, readxl, writexl, highcharter, janitor,
                tidytext, RColorBrewer, tm, grDevices, reshape2, 
                R.utils, readr, data.table, zoo, lubridate,
-               stringr)
+               stringr, Hmisc,scales, gridExtra)
+
+#####----- Base Filiacao ------#####
 
 # selecionando as colunas relevantes
 
@@ -26,14 +28,6 @@ toc() #10-13 min
 
 amostra <- sample_n(df,size = 10000,replace = F)
 
-tic("Tempo para carregar a base")
-print("Comecando a carregar a base")
-
-load("base_filiacao.RData")
-
-print("Terminou de carregar")
-toc() #6 min
-
 #save(amostra, file = "amostra_base_filiacao.RData")
 #save(df,file = "base_filiacao.RData")
 
@@ -46,9 +40,11 @@ amostra <- amostra %>%
 # conciliando as datas de desfiliacao/cancelamento (tratarei como se fossem a mesma coisa)
 
 amostra <- amostra %>%
-  mutate(data_desfiliacao = ifelse(is.na(data_desfiliacao) == T, as.Date("2021-01-01"), 
+  mutate(data_desfiliacao = ifelse(is.na(data_desfiliacao) == T & is.na(data_cancelamento) == T,
+                                   as.Date("2021-01-01"), 
                                    as.Date(data_desfiliacao)),
-         data_cancelamento = ifelse(is.na(data_cancelamento) == T, as.Date("2021-01-01"),
+         data_cancelamento = ifelse(is.na(data_cancelamento) == T & is.na(data_desfiliacao) == T,
+                                    as.Date("2021-01-01"),
                                     as.Date(data_cancelamento)),
          data_saida = ifelse(data_desfiliacao > data_cancelamento, zoo::as.Date(data_desfiliacao),
                              zoo::as.Date(data_cancelamento)))
@@ -107,40 +103,26 @@ base_ano$ano <- as.numeric(str_remove(base_ano$ano,"ano_"))
 save(base_ano,partidos_ano,"bases_tratadas.RData")
 
 
-highchart() %>%
-  hc_add_series(data=base_ano,type = 'line', hcaes(x = 'ano', y = 'num_filiados'),
-                name="Total Number of Affiliates Per Year",color="black",
-                dataLabels = list(enabled = T))%>%
-  hc_xAxis(categories=base_ano$ano) %>%
-  hc_title(
-    text = "<b> </b>",
-    margin = 20,
-    align = "center",
-    style = list(color = "dark blue", useHTML = TRUE)
-  ) 
-
-hc <- hchart(base_ano, "line", hcaes(x = ano, y = num_filiados), 
-             dataLabels = list(enabled = T), theme = "economist") %>%
-  hc_yAxis(title = list(text = "Cases")) %>%
-  hc_title(text = "Total Number of Affiliates Per Year") %>%
-  hc_add_theme(hc_theme_smpl())
-
-hc
-
-save(hc,file = "grafico_1.RData")
-
-
 source('/Users/bernardoduque/Documents/Puc/theme_publication_ggplot.R')
 
-
-library(gridExtra)
-library(scales)
 quartz()
 grafico_1 <- ggplot(base_ano,aes(ano,num_filiados))+
   geom_line(size=1.3) + geom_point() +
   xlab("Ano") + ylab("Number of Affiliates") + 
   scale_colour_brewer() + theme_clean() +
   scale_y_continuous(labels = label_number(accuracy = 1000))
+
+g1 <- ggplot(base_ano,aes(x=ano,y=num_filiados)) + 
+  geom_line(color = "dodgerblue") +
+  geom_point(aes(y=num_filiados),color = "dodgerblue") +
+  #  geom_text(aes(label = num_empreg),vjust = -0.8)+
+  xlab("Year") + ylab("Number of Affiliates") +
+  scale_y_continuous(labels = comma) +
+  theme_bw() + 
+  theme(legend.position = "none",
+        panel.grid.major.x = element_blank(),
+        panel.grid.minor.y = element_blank(),
+        axis.text = element_text(face = "bold")) 
 
 line <- ggplot(base_ano,aes(ano,num_filiados)) + 
   geom_line(size=1.3) + geom_point()
@@ -160,4 +142,88 @@ save(grafico_1,file = "grafico_1.RData")
   
 seq(from = 2013,to = as.numeric(amostra$ano_saida[5]), by = 1)
 seq(2013,ano_saida,1)
+  
+
+#####----- Base Servidores -----#####
+
+servidores <- read.csv("servidores_federais_2013-2020.csv")
+
+# limpando servidores que estao com vinculo sigiloso
+
+tipo_errado <- servidores %>%
+  filter(tipo_vinculo == -11) %>%
+  select(id_servidor) %>%
+  pull
+
+servidores <- servidores %>%
+  filter(id_servidor %nin% tipo_errado,
+         nome %nin% c("Sigiloso","SEM INFORMACAO"))
+
+# alterando coluna de tipo de vinculo
+
+servidores <- servidores %>%
+  mutate(tipo_vinculo = ifelse(tipo_vinculo == 1, "Confianca",
+                               ifelse(tipo_vinculo == 2, "Servidor","Outros")))
+
+# criando dummys para separar os anos
+
+servidores <- servidores %>%
+  mutate(comeco_emprego = as.Date(comeco_emprego),
+         fim_emprego = as.Date(fim_empregp)) %>%
+  select(-fim_empregp)
+
+
+# definindo o ano como acabando em novembro (ja que registro comeca no 1o dia do mes)
+
+servidores <- servidores %>%
+  mutate(ano_2013 = ifelse(comeco_emprego < "2013-11-01",1,0),
+         ano_2014 = ifelse(comeco_emprego < "2014-11-01" &
+                            fim_emprego >= "2013-10-01", 1,0),
+         ano_2015 = ifelse(comeco_emprego < "2015-11-01" &
+                             fim_emprego >= "2014-11-01", 1,0),
+         ano_2016 = ifelse(comeco_emprego < "2016-11-01" &
+                             fim_emprego >= "2015-11-01", 1,0),
+         ano_2017 = ifelse(comeco_emprego < "2017-11-01" &
+                             fim_emprego >= "2016-11-01", 1,0),
+         ano_2018 = ifelse(comeco_emprego < "2018-11-01" &
+                             fim_emprego >= "2017-11-01", 1,0),
+         ano_2019 = ifelse(comeco_emprego < "2019-11-01" &
+                             fim_emprego >= "2018-11-01", 1,0),
+         ano_2020 = ifelse(comeco_emprego < "2020-11-01" &
+                             fim_emprego >= "2019-11-01", 1,0))
+
+# agrupando todos os servidores
+
+total <- servidores %>%
+  group_by(id_servidor,nome,cpf) %>% 
+  summarise(comeco_emprego = min(comeco_emprego),
+            fim_emprego = max(fim_emprego),
+            across(starts_with("ano_"),sum))
+
+total <- total %>%
+  ungroup() %>%
+  mutate(across(starts_with("ano_"),~ifelse(.x > 0,1,0)))
+
+# criando base por ano
+
+total_ano <- total %>%
+  select(starts_with("ano_")) %>%
+  summarise(across(everything(),sum)) %>%
+  ungroup()
+
+total_ano <- total_ano %>%
+  pivot_longer(everything(),names_to = "ano",values_to = "num_empreg") %>%
+  mutate(ano = as.integer(str_remove(ano,"ano_")))
+
+g2 <- ggplot(total_ano,aes(x=ano,y=num_empreg)) + 
+  geom_line(color = "dodgerblue") +
+  geom_point(aes(y=num_empreg),color = "dodgerblue") +
+#  geom_text(aes(label = num_empreg),vjust = -0.8)+
+  xlab("Year") + ylab("Number of Workers") +
+  scale_y_continuous(labels = comma) +
+  theme_bw() + 
+  theme(legend.position = "none",
+        panel.grid.major.x = element_blank(),
+        panel.grid.minor.y = element_blank(),
+        axis.text = element_text(face = "bold")) 
   
